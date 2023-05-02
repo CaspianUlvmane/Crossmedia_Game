a4fe1b17f00729396a442770d82bbfb7fd3d0c2f
Caspian Ulvmåne <cabbeulv@gmail.com>
2023-05-02 16:24:27 +0200

    hazard zone is circle and logs damage taken

--- Google Maps Test/index.js
+++ Google Maps Test/index.js
@@ -43,6 +43,9 @@ function initMap() {
           draggable: false,
         });
         location_update(icon);
+        map.addListener("click", (mapsMouseEvent) => {
+          send_hazard(mapsMouseEvent, map);
+        });
       },
       () => {
         handle_location_error(true, infoWindow, map.getCenter());
@@ -72,16 +75,68 @@ function location_update(icon) {
       lng: position.coords.longitude,
     };
     icon.setPosition(pos);
+    console.log(pos);
   });
-  console.log(icon);
-  let marker_pos_div = document.querySelector("#map > div > div > div > div")
-  if(marker_pos_div){
-    console.log(marker_pos_div.offsetLeft);
-    console.log(marker_pos_div.offsetTop);
+
+  let hazard_zones_array = Array.from(
+    document.querySelectorAll(".hazard_area")
+  );
+  if (hazard_zones_array[0]) {
+    hazard_zones_array.forEach((z) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        if (hazard_zone_bool(z, pos)) {
+          console.log("damage taken");
+        }
+      });
+    });
   }
   setTimeout(() => {
     location_update(icon);
   }, 5000);
+}
+
+function hazard_zone_bool(z, player_pos) {
+  let hazard_pos = { lat: z.dataset.lat, lng: z.dataset.lng };
+  let newRadius = distanceInKmBetweenEarthCoordinates(
+    hazard_pos.lat,
+    hazard_pos.lng,
+    player_pos.lat+0.00009,
+    player_pos.lng
+  );
+
+console.log(hazard_pos);
+
+  let radius = 0.071;
+
+  console.log(newRadius);
+  console.log(radius);
+
+  return newRadius <= radius;
+}
+
+function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+  var earthRadiusKm = 6378;
+
+  var dLat = degreesToRadians(lat2 - lat1);
+  var dLon = degreesToRadians(lon2 - lon1);
+
+  lat1 = degreesToRadians(lat1);
+  lat2 = degreesToRadians(lat2);
+
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return earthRadiusKm * c;
+}
+
+function degreesToRadians(degrees) {
+  return (degrees * Math.PI) / 180;
 }
 
 function create_hazard_buttons() {
@@ -107,7 +162,7 @@ function create_hazard_buttons() {
 }
 
 function toggle_hazard(event) {
-  event.stopPropagation()
+  event.stopPropagation();
   if (event.target.classList.contains("active")) {
     event.target.classList.remove("active");
   } else {
@@ -117,25 +172,35 @@ function toggle_hazard(event) {
   }
 }
 
+function send_hazard(mapsMouseEvent, map) {
+  let hazard_div = document.querySelector(".active");
 
-function send_hazard(event) {
-  let hazard_div = document.querySelector(".active")
-  console.log(event);
-  if(hazard_div){
-    let danger_div = document.createElement("div")
-    danger_div.style.position = "absolute"
-    danger_div.style.top = event.pageY -75 +"px"
-    danger_div.style.left = event.pageX - 75 +"px"
-    danger_div.style.width = "150px" 
-    danger_div.style.height = "150px"
-    danger_div.style.zIndex = 10
-    danger_div.classList.add(hazard_div.id) 
-    document.querySelector("body").append(danger_div)
-    setTimeout(() => {danger_div.remove()}, 300000)
+let latLng = mapsMouseEvent.latLng.toString().replace("(", "").replace(")", "").replace(",", "")
+
+latLng = latLng.split(" ")
+
+console.log(latLng);
+
+  if (hazard_div) {
+    let danger_div = document.createElement("div");
+    danger_div.style.position = "absolute";
+    danger_div.style.top = mapsMouseEvent.domEvent.pageY - 75 + "px";
+    danger_div.style.left = mapsMouseEvent.domEvent.pageX - 75 + "px";
+    danger_div.dataset.lng = latLng[1];
+    danger_div.dataset.lat = latLng[0];
+    danger_div.style.width = "150px";
+    danger_div.style.height = "150px";
+    danger_div.style.zIndex = 10;
+    danger_div.classList.add("hazard_area");
+    danger_div.classList.add(hazard_div.id);
+    document.querySelector("body").append(danger_div);
+    setTimeout(() => {
+      danger_div.remove();
+    }, 300000);
   }
 }
 
 window.initMap = initMap;
 create_hazard_buttons();
 
-window.addEventListener("click", send_hazard)
+// window.addEventListener("click", send_hazard);
